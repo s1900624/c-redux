286b40bb0b7e64ae32d84fe104ff8592ca3ccfba
Your Name <you@example.com>
2019-11-26 17:43:24 +0200

    Add action.js

---
+++ src/actions.js
@@ -0,0 +1,18 @@
+export const inc = () => () => {
+    return {
+        type: 'INC'
+    }
+};
+
+export const dec = () => {
+    return {
+        type: 'DEC'
+    }
+};
+
+export const rnd = () => {
+    return {
+        type: 'RND',
+        payload: Math.floor(Math.random()*7)
+    };
+};
