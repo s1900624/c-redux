37929e4da2aa202ffea2bb090ec2b7d3b967271e
Your Name <you@example.com>
2019-11-27 13:54:19 +0200

    Updates

---
+++ precache-manifest.90c61cb0035bb57f82d5f61ae27ec749.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57d739f29e9658ef7c926055664fdc4",
+    "url": "/s1900624/c-redux/index.html"
+  },
+  {
+    "revision": "93ccc578ae2b420f966e",
+    "url": "/s1900624/c-redux/static/js/2.2788fe70.chunk.js"
+  },
+  {
+    "revision": "c4aef54aacc0cfc0eba6",
+    "url": "/s1900624/c-redux/static/js/main.394cc159.chunk.js"
+  },
+  {
+    "revision": "5b47e825868d19016cec",
+    "url": "/s1900624/c-redux/static/js/runtime-main.8f6b651d.js"
+  }
+]);
